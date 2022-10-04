150ac17adfcf846ac8c1d06bde645ad824cd9782
Patrobas Bwire <patrobasbwire@gmail.com>
2022-10-04 13:08:04 +0300

    added a section and a number of divs in ContactList component

--- src/components/contacts/ContactList/ContactList.js
+++ src/components/contacts/ContactList/ContactList.js
@@ -19,14 +19,40 @@ let ContactList = () => {
               </div>
               <div className="row">
                 <div className="col-md-6">
-                  <form>
-                      <div className="mb-2">
-                        <input type="text" className="form-control" placeholder="Search Names"/>
-                      </div>
-                      <div className="mb-2">
-                      <input type="Submit" className="btn btn-outline-dark" value="Search"/>
-                      </div>
+                  <form className="row">
+                    <div className="col">
+                        <div className="mb-2">
+                            <input type="text" className="form-control" placeholder="Search Names"/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="mb-2">
+                            <input type="Submit" className="btn btn-outline-dark" value="Search"/>
+                        </div>
+                    </div>
                   </form>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+
+      <section className="contact-list">
+        <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="col-md-4">
+                      <img src="https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png" alt="" className="contact-img"/>
+                    </div>
+                    <div className="col-md-7">
+
+                    </div>
+                    <div className="col-md-1">
+
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
