e6f837bb5f74fb7b7e9a4aba5774c77f0d001423
Patrobas Bwire <patrobasbwire@gmail.com>
2022-10-06 13:09:39 +0300

    succesfuly added the required codes to editcContat component that enabled the edit page to update contacts succesfully

--- src/components/contacts/EditContact/EditContact.js
+++ src/components/contacts/EditContact/EditContact.js
@@ -1,8 +1,11 @@
 import React, { useState , useEffect} from 'react';
-import {json, Link, useParams} from 'react-router-dom';
+import {Link, useParams , useNavigate} from 'react-router-dom';
 import {ContactService} from '../../../Services/ContactService';
+import Spinner from "../../Spinner/Spinner";
 
 let EditContact = () => {
+
+  let navigate = useNavigate();
 
   let {contactId} = useParams();
 
@@ -27,11 +30,13 @@ let EditContact = () => {
       try{
         setState({...state, loading: true});
         let response = await ContactService.getContact(contactId);
+        let groupResponse = await ContactService.getGroups();
         console.log(response.data)
         setState({
               ...state,
               loading: false,
-              contact: response.data
+              contact: response.data,
+              groups: groupResponse.data
         });
       }
       catch (error) {
@@ -44,8 +49,29 @@ let EditContact = () => {
     })();
   }, [contactId]);
 
-  let upDateInput = () => {
+  let upDateInput = (event) => {
+    setState ({
+      ...state, 
+      contact:{
+        ...state.contact,
+        [event.target.name] : event.target.value
+      }
+    });
+  };
 
+  let submitForm = async (event) => {
+      event.preventDefault();
+      try {
+        let response = await ContactService.updateContact(state.contact, contactId);
+        if(response) {
+          navigate('/contacts/list', {replace: false});
+        }
+      }
+      catch (error) {
+        setState({...state, errorMessage: error.message});
+        navigate(`/contacts/edit/${contactId}`, {replace: false})
+        
+      }
   };
 
   let {loading , contact , groups , errorMessage} = state;
@@ -53,7 +79,8 @@ let EditContact = () => {
 
     return (
       <React.Fragment>
-        <pre>{JSON.stringify(contact)}</pre>
+        {
+          loading ? <spinner/> : <React.Fragment>
       <section className='add-contact p-3'>
        <div className='container'>
          <div className='row'>
@@ -64,30 +91,71 @@ let EditContact = () => {
          </div>
          <div className='row align-items-center'>
            <div className='col-md-4'>
-             <form>
+             <form onSubmit={submitForm}>
                <div className='mb-2'>
                  <input  
+                  required ="true"
+                  name = 'name'
                   value={contact.name}
+                  onChange={upDateInput}
                   type='text' className='form-control' placeholder='Name'/>
                </div>
                <div className='mb-2'>
-                 <input  type='text' className='form-control' placeholder='photo Url'/>
+                 <input  
+                  required ="true"
+                  name = 'photo'
+                  value={contact.photo}
+                  onChange={upDateInput}
+                  type='text' className='form-control' placeholder='photo Url'/>
                </div>
                <div className='mb-2'>
-                 <input  type='number' className='form-control' placeholder='Mobile'/>
+                 <input 
+                  required ="true"
+                  name = 'mobile'
+                  value={contact.mobile}
+                  onChange={upDateInput}
+                  type='number' className='form-control' placeholder='Mobile'/>
                </div>
                <div className='mb-2'>
-                 <input  type='email' className='form-control' placeholder='Email'/>
+                 <input 
+                  required ="true"
+                  name = 'email'
+                  value={contact.email}
+                  onChange={upDateInput}
+                  type='email' className='form-control' placeholder='Email'/>
                </div>
                <div className='mb-2'>
-                 <input  type='text' className='form-control' placeholder='Company'/>
+                 <input 
+                  required ="true"
+                  name = 'company'
+                  value={contact.company}
+                  onChange={upDateInput}
+                  type='text' className='form-control' placeholder='Company'/>
                </div>
                <div className='mb-2'>
-                 <input  type='text' className='form-control' placeholder='Title'/>
+                 <input  
+                  required ="true"
+                  name = 'title'
+                  value={contact.title}
+                  onChange={upDateInput}
+                  type='text' className='form-control' placeholder='Title'/>
                </div>
                <div className='mb-2'>
-                 <select className='form-control'>
+                 <select 
+                  required ="true"
+                  name = 'groupId'
+                  value={contact.groupId}
+                  onChange={upDateInput}
+                  className='form-control'>
                    <option value=""> Select a Group</option>
+                   {
+                    groups.length > 0 &&
+                    groups.map(group => {
+                      return (
+                        <option key={group.id} value={group.id}>{group.name}</option>
+                      )
+                    })
+                   }
                  </select>
                </div>
                <div className='mb-2'>
@@ -98,11 +166,13 @@ let EditContact = () => {
              </form>
            </div>
            <div className='col-md-6'>
-            <img src='https://cdn-icons-png.flaticon.com/512/747/747376.png' alt='' className='img-fluid contact-img'/>
+            <img src={contact.photo} alt='' className='img-fluid contact-img'/>
            </div>
          </div>
        </div>
       </section>
+            </React.Fragment>
+        }
    </React.Fragment>
     )
 };
